8e4879ab602fbfb6cb22c54981a237805472d5a3
Your Name <you@example.com>
2021-09-28 01:53:56 -0400

    m

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 go build -o bashful .
 rsync bashful ~/.local/bin/bashful
 rsync bashful $(which bashful)
-rsync bashful ~/vpntech-haproxy-container/files/bashful
+[[ -d ~/vpntech-haproxy-container/files ]] && rsync bashful ~/vpntech-haproxy-container/files/bashful
