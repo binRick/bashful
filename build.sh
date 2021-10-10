6a850f140c494d4c77a34c0e7817c599b6fa7b01
Your Name <you@example.com>
2021-10-10 11:44:07 -0400

    m

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 ./compile.sh
 rsync bashful ~/.local/bin/bashful
 rsync bashful $(which bashful)
+rsync bashful /usr/bin/bashful
 [[ -d ~/vpntech-haproxy-container/files ]] && rsync bashful ~/vpntech-haproxy-container/files/bashful
 [[ -d /opt/vpntech-binaries/x86_64 ]] && rsync bashful /opt/vpntech-binaries/x86_64/bashful
