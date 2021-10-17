4c55edc6a805ed7b9d4ec39df3f457d44d3664a5
root <root@f111.vpnservice.company>
2021-10-16 23:42:42 -0400

    m

--- compile.sh
+++ compile.sh
@@ -3,5 +3,5 @@ set -e
 cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 go build -o bf .
-rsync bf bashful
-[[ -d dist ]] && rsync bashful dist/.
+cp -prvf bf bashful
+[[ -d dist ]] && cp -prvf bashful dist/bashful
