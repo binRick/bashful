3c0d88367d4b81de885542b106d71dd4a1fcd923
Your Name <you@example.com>
2021-10-16 23:38:22 -0400

    m

--- test.sh
+++ test.sh
@@ -1,1 +1,31 @@
-./test_forks.sh example/00-demo.yml --only-tags t
+#!/bin/bash
+set -e 
+f="${1:-example/00-demo.yml}"; shift||true
+a="${@:---only-tags t}"
+go build -o ./bf .
+err=$(mktemp)
+dorun(){
+  (
+  reap ./bf run $f $a || \
+    reap ./bf run example/05-minimal.yml
+  )
+}
+dorun 2> .ee || cat .ee
+#| tee $err | tee .ee || cat $err
+
+unlink $err
+
+exit
+
+
+set +e
+extrace -Ql -o .e passh ./bf run $f $a
+
+
+
+if [[ -f .e ]]; then 
+echo -ne "forks: "; wc -l .e;  echo -ne "sttys: "; grep -c stty .e; 
+
+trap "unlink .e" EXIT
+
+fi
--- test_forks.sh
+++ test_forks.sh
@@ -4,28 +4,27 @@ f="${1:-example/00-demo.yml}"; shift||true
 a="${@:---only-tags t}"
 go build -o ./bf .
 err=$(mktemp)
-dorun(){
-  (
-  reap ./bf run $f $a || \
-    reap ./bf run example/05-minimal.yml
-  )
-}
-dorun 2> .ee || cat .ee
-#| tee $err | tee .ee || cat $err
 
-unlink $err
+pfx="extrace -Ql -o .e passh reap"
+cmd_run="./bf run"
+cmd_run="$pfx $cmd_run"
 
-exit
-
-
-set +e
-extrace -Ql -o .e passh ./bf run $f $a
-
-
-
+cc(){
 if [[ -f .e ]]; then 
 echo -ne "forks: "; wc -l .e;  echo -ne "sttys: "; grep -c stty .e; 
 
 trap "unlink .e" EXIT
 
 fi
+}
+dorun(){
+  (
+    eval $cmd_run $f $a || \
+    eval $cmd_run example/05-minimal.yml
+  )
+}
+
+
+trap cc EXIT
+dorun 2> .ee || cat .ee
+unlink $err
