f571d58eb7c04e08d75f78fd16a82da8e1db4f85
root <root@f111.vpnservice.company>
2021-10-16 23:46:31 -0400

    m

--- compile.sh
+++ compile.sh
@@ -3,5 +3,8 @@ set -e
 cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 go build -o bf .
+[[ -f bashful ]] && unlink bashful
+[[ -f dist/bashful ]] && unlink dist/bashful
 cp -prvf bf bashful
+set +e
 [[ -d dist ]] && cp -prvf bashful dist/bashful
