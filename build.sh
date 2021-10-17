162112381cb69fe791e9bb3dd9bf1d0d0ae04b4c
root <root@f111.vpnservice.company>
2021-10-16 23:51:33 -0400

    m

--- build.sh
+++ build.sh
@@ -3,8 +3,14 @@ set -e
 cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 ./compile.sh
+if command -v rsync; then
 rsync bashful ~/.local/bin/bashful
 rsync bashful $(which bashful)
 rsync bashful /usr/bin/bashful
 [[ -d ~/vpntech-haproxy-container/files ]] && rsync bashful ~/vpntech-haproxy-container/files/bashful
 [[ -d /opt/vpntech-binaries/x86_64 ]] && rsync bashful /opt/vpntech-binaries/x86_64/bashful
+else
+	cp bashful /usr/bin/bashful
+fi
+
+true
