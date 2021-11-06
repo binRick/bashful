74cc1675f24d862a0837c4ffb3e6711775ce142b
rick <rick@ricks-MacBook-Air.local>
2021-11-06 12:11:19 -0400

    m

--- build.sh
+++ build.sh
@@ -4,11 +4,19 @@ cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 ./compile.sh
 if command -v rsync; then
-	rsync bashful ~/.local/bin/bashful
-	rsync bashful $(which bashful)
-	rsync bashful /usr/bin/bashful
+  if [[ -d ~/.local/bin ]]; then
+  	rsync bashful ~/.local/bin/bashful
+  fi
+  if uname -s |grep -i darwin; then
+    echo darwin
+  else
+  if command -v bashful; then
+  	rsync bashful $(command -v bashful)
+  fi
+	rsync bashful /usr/bin/bashful||true
 	[[ -d ~/vpntech-haproxy-container/files ]] && rsync bashful ~/vpntech-haproxy-container/files/bashful
 	[[ -d /opt/vpntech-binaries/x86_64 ]] && rsync bashful /opt/vpntech-binaries/x86_64/bashful
+  fi
 else
 	cp bashful /usr/bin/bashful
 fi
