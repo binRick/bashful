ee8949f4aeaa8f1de0410d424c8371f3aeb6ef07
root <root@f35.vpnservice.company>
2021-11-21 17:31:11 -0500

    m

---
+++ dev-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+EXEC_ARGS="${@:-echo OK}"
+cmd="nodemon -w . -e sh,go,yml,mod,go,j2,txt -I --signal SIGKILL -x env -- bash -c 'reset && ./build.sh||true; $EXEC_ARGS'"
+
+git_pull() {
+	(
+		set +e
+		while :; do
+			git pull || git pull
+			sleep 15
+		done
+	) &
+
+}
+
+# && passh ./test_forks.sh $@ && { set +e; killall bashful 2>/dev/null||killall -9 bashful 2>/dev/null; killall bf 2>/dev/null; }'"
+
+git_pull
+eval $cmd
