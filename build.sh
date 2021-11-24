84711f0a819a08dc7d49f68d27436ba172957cd0
root <root@f35.vpnservice.company>
2021-11-24 12:00:28 -0500

    add bash loadables prep work

--- build.sh
+++ build.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 set -e
 cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+BV=5.1
+BL=$(pwd)/bash-loadables
+BASH_LOADABLES_DIR=$BL/bash-$BV/examples/loadables
+
+[[ -d "$BL" ]] || mkdir -p "$BL"
+command -v bison >/dev/null || dnf -y install bison
+
+if [[ ! -f $BL/bash-$BV/bash ]]; then
+  (
+    cd $BL/.
+    tar zxf ../src/bash-$BV.tar.gz
+    mv bash-bash-$BV bash-$BV
+    cd bash-$BV
+    { ./configure &&  make; } |  pv -l -N "Compiling Bash v$BV"  >/dev/null
+  )
+fi
+
+if [[ ! -f $BL/color.so ]]; then
+  ./submodules/bash-loadable-ansi-color/build.sh
+  rsync submodules/bash-loadable-ansi-color/src/.libs/color.so $BL/.
+fi
+
+./GET_BASH_LOADABLES.sh build_modules|tr '\n' ' '
+tf=$(mktemp)
+./GET_BASH_LOADABLES.sh get_built_modules > $tf
+cmd="rsync --files-from=$tf $BASH_LOADABLES_DIR/. $BL/. -v"
+eval "$cmd"
+unlink $tf
 
 ./compile.sh
+
 if command -v rsync; then
   if [[ -d ~/.local/bin ]]; then
   	rsync bashful ~/.local/bin/bashful
